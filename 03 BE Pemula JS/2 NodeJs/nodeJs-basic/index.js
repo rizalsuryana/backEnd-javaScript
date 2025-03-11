29c84b747aac5589aee98a431bfa99029657ef76
rizalsuryana <rizal.suryn@gmail.com>
2025-03-11 12:51:27 +0700

    aw

---
+++ 03 BE Pemula JS/2 NodeJs/nodeJs-basic/index.js
@@ -0,0 +1,5 @@
+const message = (naem) => {
+    console.log(`Hello ${naem}`)
+}
+
+message('JavScript');
---
+++ JS/final-assessment/01-writing-comments/index.js
@@ -0,0 +1,9 @@
+// rizalsuryana
+/**
+ * Goals Tahun ini :
+ * Dapat kerja jadi Front-End Developer
+ * Jago ngoding, jadi FE & React Dev, jadi BE juga gpp, karena Front_end hanyalah judul
+ * pas dibaca spesifikasi tetap mintanya banyakan ke BackEnd :(
+ * 
+ * Please Hire Me ... I Need a job :)
+ */

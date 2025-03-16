2d2124c0da5bdc1c102ebc03ecbd2a40dddb7591
rizalsuryana <rizal.suryn@gmail.com>
2025-03-16 21:20:48 +0700

    routing request

--- 03 BE Pemula JS/04 NodeJS-WebServices/1 web-server/server.js
+++ 03 BE Pemula JS/04 NodeJS-WebServices/1 web-server/server.js
@@ -4,24 +4,39 @@ const http = require('http');
 const requestListener = (request, response) => {
 response.setHeader('Content-Type', 'text/html');
 response.statusCode = 200;
-const { method } = request;
+const { method, url } = request;
 
-if(method === 'GET') {
-    response.end('<h1>GET request received</h1>');
+if(url === '/') {
+    // TODO 2: Logika respons bila url bernilai '/'
+    if(method === 'GET') {
+        response.end('<h1>Ini adalah homepage</h1>')
+    } else {
+        response.end(`Halaman tidak dapat diakses dengan ${method} request`)
+    }
+
+} else if(url === '/about') {
+    // TODO 3: Logika respons bila url bernilai '/about'
+    if(method === 'GET') {
+        response.end('<h1> Hallo ini adalah halaman about </h1>');
+    } else if(method=== 'POST') {
+        let body = [];
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        request.on('end', () => {
+            body = Buffer.concat(body).toString();
+            const { name } = JSON.parse(body);
+            response.end(`<h1>Hai ${name}! ini adalah halaman about</h1>`)
+        })
+     } else {
+        response.end(`<h1>Halaman ini tidak datap diakses dengan ${method} request</h1>`)
+     }
+
+
+} else {
+    // TODO 1: Logika respons bila url bukan '/' atau '/about'
+    response.end('<h1>Halaman tidak ditemukan !</h1>')
 }
-if(method === 'POST'){
-    let body = [];
-    request.on('data', (chunk) => {
-        body.push(chunk);
-    });
-
-    request.on('end', () => {
-        body = Buffer.concat(body).toString();
-        const { name } = JSON.parse(body);
-        response.end(`<h1>Hai, ${name}</h1>`)  
-    })
-}
-
 
 }
 
@@ -33,3 +48,23 @@ const host = 'localhost';
 server.listen(port, host,() => {
     console.log(`Server running on http://${host}:${port}`);
 })
+
+
+
+
+
+// URL: ‘/’
+// Method: GET
+// Mengembalikan “Ini adalah homepage”.
+// Method: <any> (selain GET)
+// Mengembalikan “Halaman tidak dapat diakses dengan <any> request”.
+// URL: ‘/about’
+// Method: GET
+// Mengembalikan “Halo! Ini adalah halaman about”.
+// Method: POST (dengan melampirkan data name pada body)
+// Mengembalikan “Halo, <name>! Ini adalah halaman about”.
+// Method: <any> (selain GET dan POST)
+// Mengembalikan “Halaman tidak dapat diakses dengan <any> request”.
+// URL: <any> (selain / dan /about)
+// Method: <any>
+// Mengembalikan “Halaman tidak ditemukan!”.

3558138c5e8ebcf339aa87c38f77eef1058b0d9b
rizalsuryana <rizal.suryn@gmail.com>
2025-03-16 13:42:54 +0700

    server

---
+++ 03 BE Pemula JS/04 NodeJS-WebServices/1 web-server/server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+/**
+ * Logika untuk menangani dan menanggapi request dituliskan pada fungsi ini
+ * 
+ * @param request: objek yang berisikan informasi terkait permintaan
+ * @param response: objek yang digunakan untuk menanggapi permintaan
+ */
+
+
+const requestListener = (request, response) => {
+response.setHeader('Content-Type', 'text/html');
+
+response.statusCode = 200;
+response.end('<h1>Hallo Http Server :) slebeww</h1>')
+}
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = 'localhost';
+
+server.listen(port, host,() => {
+    console.log(`Server running on http://${host}:${port}`);
+})

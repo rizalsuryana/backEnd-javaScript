283e360c2bd54689d7d29ec537f8f7a51eddcb4c
rizalsuryana <rizal.suryn@gmail.com>
2025-03-17 18:03:44 +0700

    response status

--- 03 BE Pemula JS/04 NodeJS-WebServices/1 web-server/server.js
+++ 03 BE Pemula JS/04 NodeJS-WebServices/1 web-server/server.js
@@ -3,20 +3,25 @@ const http = require('http');
 
 const requestListener = (request, response) => {
 response.setHeader('Content-Type', 'text/html');
-response.statusCode = 200;
 const { method, url } = request;
 
 if(url === '/') {
     // TODO 2: Logika respons bila url bernilai '/'
     if(method === 'GET') {
         response.end('<h1>Ini adalah homepage</h1>')
+        // status code
+        response.statusCode = 200;
     } else {
+     // status code
+     response.statusCode = 400;
         response.end(`Halaman tidak dapat diakses dengan ${method} request`)
     }
 
 } else if(url === '/about') {
     // TODO 3: Logika respons bila url bernilai '/about'
     if(method === 'GET') {
+        // status code
+        response.statusCode = 200;
         response.end('<h1> Hallo ini adalah halaman about </h1>');
     } else if(method=== 'POST') {
         let body = [];
@@ -26,14 +31,20 @@ if(url === '/') {
         request.on('end', () => {
             body = Buffer.concat(body).toString();
             const { name } = JSON.parse(body);
+                    // status code
+        response.statusCode = 200;
             response.end(`<h1>Hai ${name}! ini adalah halaman about</h1>`)
         })
      } else {
+             // status code
+     response.statusCode = 400;
         response.end(`<h1>Halaman ini tidak datap diakses dengan ${method} request</h1>`)
      }
 
 
 } else {
+                 // status code
+                 response.statusCode = 404;
     // TODO 1: Logika respons bila url bukan '/' atau '/about'
     response.end('<h1>Halaman tidak ditemukan !</h1>')
 }
